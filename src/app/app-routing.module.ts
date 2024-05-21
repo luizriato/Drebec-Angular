773ad5ad61b358de85d02054538b5fc9a22311c3
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-21 18:32:12 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from "./home/home.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 
 const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'Home', component: HomeComponent},
     {path: 'Sobre', component: AboutUsComponent}, 
     {path: 'Contatos', component: AboutUsComponent},
     {path: 'Login', component: AboutUsComponent},
--- src/app/simulador/simulador.component.ts
+++ src/app/simulador/simulador.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-simulador',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './simulador.component.html',
   styleUrl: './simulador.component.css'
 })
