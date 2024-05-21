81b3617369e41757aec770bf95ac169ad747956a
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-21 17:08:42 -0300

    update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'Sobre', component: AboutUsComponent}, 
+    {path: 'Contatos', component: AboutUsComponent},
+    {path: 'Login', component: AboutUsComponent},
+    {path: 'Simulador', component: AboutUsComponent},
+]
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
