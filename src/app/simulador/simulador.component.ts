c17910e2a005240a67ca659b7c2d674029197782
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-21 17:06:52 -0300

    update

---
+++ src/app/simulador/simulador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulador',
+  standalone: true,
+  imports: [],
+  templateUrl: './simulador.component.html',
+  styleUrl: './simulador.component.css'
+})
+export class SimuladorComponent {
+
+}
