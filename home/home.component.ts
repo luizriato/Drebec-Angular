8f6da199ff9c4d605f98ddc997cc5f5f07d49ef2
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-23 19:04:11 -0300

    Add files via upload

---
+++ home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink, RouterModule], 
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
