adf3fdf3325fb1ee8dfe3401e560570a439be32c
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-23 19:04:47 -0300

    Add files via upload

---
+++ simulador/simulador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulador',
+  standalone: true,
+  imports: [],
+  templateUrl: './simulador.component.html',
+  styleUrl: './simulador.component.scss'
+})
+export class SimuladorComponent {
+
+}
