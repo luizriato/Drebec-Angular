60e647b9f1f99375a8e27ca5c0a694cf681b256e
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:43:02 -0300

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
@@ -10,19 +9,4 @@ import { RouterLink, RouterModule } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-=======
-import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [RouterLink], 
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
-})
-export class HomeComponent {
-
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
 }
