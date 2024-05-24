cb286a06840984dee3cf94b3411b120bd039c357
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:43:56 -0300

    Update about-us.component.ts

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,17 +10,3 @@ import { Component } from '@angular/core';
 export class AboutUsComponent {
 
 }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-us',
-  standalone: true,
-  imports: [],
-  templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
-})
-export class AboutUsComponent {
-
-}
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
