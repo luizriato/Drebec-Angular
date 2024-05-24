c1473263c428350d34d6ee5e1b2a28978c7f8c97
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:42:28 -0300

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,17 +10,3 @@ import { Component } from '@angular/core';
 export class LoginComponent {
 
 }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
-export class LoginComponent {
-
-}
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
