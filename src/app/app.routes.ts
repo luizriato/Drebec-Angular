854dced8e4a4f36761e615ed61ee632022c33f69
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 01:07:22 -0300

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { SimuladorComponent } from './simulador/simulador.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [{path: 'simulador', component: SimuladorComponent},
+{path: 'sobre', component: AboutUsComponent},
+{path: '', component: HomeComponent},
+{path: 'login', component: LoginComponent},
+{path: 'home', component: HomeComponent}
+];
