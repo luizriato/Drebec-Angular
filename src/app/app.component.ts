7a34e0009eabdea7b01cc29e63066c4f870e68b1
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:39:24 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
@@ -14,20 +13,3 @@ import { FooterComponent } from './footer/footer.component';
 export class AppComponent {
   title = 'bs';
 }
-=======
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent {
-  title = 'bs';
-}
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
