3aed1e367a3125cb398d1a085708232845873686
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:43:40 -0300

    Update footer.component.ts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -12,18 +11,3 @@ import { RouterLink } from '@angular/router';
 export class FooterComponent {
 
 }
-=======
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
-})
-export class FooterComponent {
-
-}
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
