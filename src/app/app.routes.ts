4596fd445dc71d574f44113c1f0d7c0caea79d73
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-24 16:39:46 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { SimuladorComponent } from './simulador/simulador.component';
@@ -12,18 +11,3 @@ export const routes: Routes = [{path: 'simulador', component: SimuladorComponent
 {path: 'login', component: LoginComponent},
 {path: 'home', component: HomeComponent}
 ];
-=======
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
-import { SimuladorComponent } from './simulador/simulador.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [{path: 'simulador', component: SimuladorComponent},
-{path: 'sobre', component: AboutUsComponent},
-{path: '', component: HomeComponent},
-{path: 'login', component: LoginComponent},
-{path: 'home', component: HomeComponent}
-];
->>>>>>> 854dced8e4a4f36761e615ed61ee632022c33f69
