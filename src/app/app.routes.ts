a868f59b49d591f96a4b6e63bb2cea83ae68ef54
luizriato <142940208+luizriato@users.noreply.github.com>
2024-05-28 19:00:18 -0300

    update
    update de páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { SimuladorComponent } from './simulador/simulador.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { TesteComponent } from './teste/teste.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 export const routes: Routes = [{path: 'simulador', component: SimuladorComponent},
 {path: 'sobre', component: AboutUsComponent},
 {path: '', component: HomeComponent},
 {path: 'login', component: LoginComponent},
-{path: 'home', component: HomeComponent}
+{path: 'home', component: HomeComponent},
+{path: 'teste', component: TesteComponent},
+{path: 'contatos', component: ContatosComponent}
 ];
