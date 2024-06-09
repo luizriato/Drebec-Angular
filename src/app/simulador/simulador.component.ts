8c75acf79790b23f12065ec0ed9b8bf5856abf9d
luizriato <142940208+luizriato@users.noreply.github.com>
2024-06-08 23:40:45 -0300

    update

--- src/app/simulador/simulador.component.ts
+++ src/app/simulador/simulador.component.ts
@@ -1,53 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-simulador',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatDialogModule],
   templateUrl: './simulador.component.html',
   styleUrl: './simulador.component.scss'
 })
-export class SimuladorComponent implements OnInit {
-  public solar: boolean = false;
-  public eolica: boolean = false;
-  public biomassa: boolean = false;
 
-  alterarVisibilidade(tamanho: string, tipo: string): void {
-    this.resetFlags();
-    if (tipo === 'casa') {
-      if (tamanho === 'pequena') {
-        this.solar = true;
-      } else if (tamanho === 'media') {
-        this.solar = true;
-        this.biomassa = true;
-      } else if (tamanho === 'grande') {
-        this.solar = true;
-        this.eolica = true;
-        this.biomassa = true;
-      }
-    } else if (tipo === 'apartamento') {
-      if (tamanho === 'media' || tamanho === 'grande') {
-        this.solar = true;
-      }
-    } else if (tipo === 'rural') {
-      if (tamanho === 'pequena') {
-        this.solar = true;
-      } else if (tamanho === 'media') {
-        this.biomassa = true;
-      } else if (tamanho === 'grande') {
-        this.eolica = true;
-      }
-    }
+
+export class SimuladorComponent {
+  constructor(public dialog: MatDialog) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
   }
-ngOnInit(): void {
-    
-}
-  resetFlags(): void {
-    this.solar = false;
-    this.eolica = false;
-    this.biomassa = false;
-  }
+
+
 }
 
-
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'dialog-content-example-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+export class DialogContentExampleDialog { }
