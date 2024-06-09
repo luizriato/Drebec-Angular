8260f282b311fdf6ee2b06d19a16081c9108f271
luizriato <142940208+luizriato@users.noreply.github.com>
2024-06-09 19:47:44 -0300

    update

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4FZV4O4O.mjs";import"./chunk-NAHZG3DV.mjs";import"./chunk-XERTXOKA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EVFA5EAB.mjs";import"./chunk-AXXPS47D.mjs";import"./chunk-33FK5BJ3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
